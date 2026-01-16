0280e0ce54f328c819b093910d62401099ca4373
Mike Willey <mikewilley@gmail.com>
2026-01-16 14:25:43 -0500

    add sponsor rotator back

--- app/page.tsx
+++ app/page.tsx
@@ -56,6 +56,9 @@ function PortfolioHome() {
     <main className="min-h-screen bg-gradient-to-b from-slate-50 to-white">
       <div className="mx-auto flex max-w-5xl flex-col gap-8 px-4 pb-16 pt-10">
         <FadeIn delay={0}>
+          <SponsorRotator sponsors={sponsors} visibleCount={2} intervalMs={8000} />
+        </FadeIn>
+        <FadeIn delay={0.1}>
           <HomeHeroSection />
         </FadeIn>
       </div>
